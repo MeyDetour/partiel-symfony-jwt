d3f7032dd80724beb3931d879ff8e18c90e2831a
mey <meydetour@gmail.com>
2024-12-17 10:22:40 +0100

    add image as response in get alls usersx

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,13 +13,70 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         $userRoute = [
+            [
+                'name' => 'Register',
+                'route' => '/register',
+                'methode' => 'POST',
+                'body' => [
+                    ["displayName" => "string (NOT NUL)",
+                        "email" => "string (NOT NUL)",
+                        "password" => "string (NOT NUL)"
+                    ]
+                ],
+                'sendBack' => ["message" => "ok"],
+                'token' => false
+            ],
+            [
+                'name' => 'Login',
+                'route' => '/api/login_check',
+                'methode' => 'POST',
+                'body' => [
+                    [
+                        "username" => "string (NOT NUL)",
+                        "password" => "string (NOT NUL)"
+                    ]
+                ],
+                'sendBack' => ["token" => "string"],
+                'token' => false
+            ],
+            [
+                'name' => 'Get all users',
+                'route' => '/api/users',
+                'methode' => 'GET',
+                'body' => [  ],
+                'sendBack' =>  [
+                    "id" => "int (AI) ",
+                    "email" => "string",
+                    "createdAt" => "string (d.m.Y h:i)",
+                    "profile" => [
+                        "id" =>"int (AI) ",
+                        "displayName" => "string"
+                    ],["..."]
+                ],
+                'token' => false
+            ]
+
 
         ];
         $eventRoute = [];
         $invitationRoute = [];
         $suggestionRoute = [];
 
-
+        /*
+        [
+                'name' => 'Register',
+                'route' => '/register',
+                'methode' => 'POST',
+                'body' => [
+                    ["displayName" => "string",
+                        "email" => "string",
+                        "password" => "string"
+                    ]
+                ],
+                'sendBack' => ["message"=>"ok"],
+                'token' => true
+            ]
+        */
 
         $themes = [
             "User" => $userRoute,
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Service\ImageService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,9 +12,19 @@ use Symfony\Component\Routing\Attribute\Route;
 class UserController extends AbstractController
 {
     #[Route('/users', name: 'get_users')]
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, ImageService $imageService): Response
     {
+        $users = $userRepository->findAll();
+        foreach ($users as $user){
+            $profile = $user->getProfile();
+            if ($profile->getImage()) {
+                $imageUrl = $imageService->getImageUrl($profile->getImage(), "profile_image");
+                $profile->setImageUrl($imageUrl);
+            }
+        }
 
-        return $this->json(  $userRepository->findAll(),200,[],["groups"=>"users"]);
+
+
+        return $this->json( $users ,200,[],["groups"=>"users"]);
     }
 }
